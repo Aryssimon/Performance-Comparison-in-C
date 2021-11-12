1d721eba700efb217c45197c3c5825815627db26
Simon Arys <simon.arys@student.uclouvain.be>
2021-11-12 11:46:24 +0100

    Add libraries and declare pthreads

--- philosophes.c
+++ philosophes.c
@@ -1,3 +1,15 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+const int CYCLES = 10000;
+
+pthread_t *phil;
+pthread_mutex_t *baguette;
+
 int main(int argc, char *argv[]) {
-  int N = atoi(argc[1]);
+  int N = atoi(argv[1]);
+  pthread_t init_phil[N];
+  pthread_mutex_t init_baguette[N];
+  phil = init_phil;
+  baguette = init_baguette;
 }
